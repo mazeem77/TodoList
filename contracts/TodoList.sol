796cc23c7f30f2ae63fe8c1c380c98d0716ca903
mazeem77 <mazeem8856@gmail.com>
2022-09-09 15:48:38 +0500

    New Solidity File

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,4 +1,5 @@
 pragma solidity ^0.5.0;
+pragma experimental ABIEncoderV2;
 
 contract TodoList{
   uint count = 0;
@@ -9,5 +10,19 @@ contract TodoList{
 
   function getCount() public view returns(uint){
     return count;
+  }
+
+  struct tdList{
+    string task;
+  }
+
+  mapping(uint => tdList) public listTd;
+
+  function setList(uint _id, string memory _task) public {
+    listTd[_id] =  tdList(_task);
+  }
+
+  function getList() public view returns(tdList memory){
+    return listTd[0];
   }
 }
